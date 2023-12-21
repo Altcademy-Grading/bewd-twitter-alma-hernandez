56624fa35af5fb1515d60530db0435febe07ba1f
alma-hernandez <1984ahern@gmail.com>
2023-12-20 19:20:28 -0600

    attritbues, validations, and relationships in. need to put attribute in db:migrate when it exists

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,11 @@
 class Tweet < ApplicationRecord
+    t.string :message
+    t.interet :user_id
+
+    t.timestamps
+
+    belongs_to :user
+
+    validates :user_id, presence: true
+    validates :message, presence: true, length {maximum: 140}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+    t,string :username
+    t.string :email
+    t.string :password
+
+    t.timestamps
+
+
+    has_many :sessions
+    has_many :tweets
+
+    validates :username, presence: true, ;ength: {minimum:3, maximum 63}
+    validates :password, presence: true
+    validates :email, presence, true
+
 end
