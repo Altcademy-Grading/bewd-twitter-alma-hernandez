cd63e45326982a0821432626a56eea86580753d9
alma-hernandez <1984ahern@gmail.com>
2023-12-21 12:47:12 -0600

    endpoints complete

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,19 @@ Rails.application.routes.draw do
   get '/feeds' => 'feeds#index'
 
   # USERS
+  post '/users', to: 'users#create'
+  
 
   # SESSIONS
+  post '/sessions', to: 'sessions#create'
+  delete '/sessions', to: 'sessions#destroy'
+  get '/authenticated', to: 'sessions#authenticated'
 
   # TWEETS
+  post '/tweets', to: 'tweets#create'
+  delete '/tweets', to: 'tweets#destroy'
+  get '/tweets', to: 'tweets#index'
+  get '/users/:username/tweets', to: 'tweets#index_by_user'
 
   # Redirect all other paths to index page, which will be taken over by AngularJS
   get '*path' => 'homepage#index'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_12_21_175336) do
+ActiveRecord::Schema.define(version: 2023_12_21_183755) do
+
+  create_table "homepages", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "sessions", force: :cascade do |t|
     t.string "token"
